3670d05752c087da7213e6566d9d481363979cd7
fcosta_oliveira <filipecosta90@gmail.com>
2019-06-19 13:47:32 +0100

    [add] using fe2s time marking (https://github.com/RedisLabs/spark-redis/blob/master/src/test/scala/com/redislabs/provider/redis/RedisBenchmarks.scala)

--- src/main/scala/com/fcosta_oliveira/App.scala
+++ src/main/scala/com/fcosta_oliveira/App.scala
@@ -1,5 +1,9 @@
 package com.fcosta_oliveira
 
+
+import java.io.{File, FileWriter, PrintWriter}
+import java.time.{Duration => JDuration}
+
 import org.apache.spark.sql.SparkSession
 
 
@@ -8,7 +12,14 @@ import org.apache.spark.sql.SparkSession
   */
 object App {
 
+  val benchmarkReportDir = new File("target/reports/benchmarks/")
+
   def main(args: Array[String]) {
+
+    val recordName = "data/10K_records_100cols_36_bytes.csv"
+    val tableName = "10K_records_100cols_36_bytes"
+    benchmarkReportDir.mkdirs()
+
 
     val spark = SparkSession
       .builder()
@@ -18,8 +29,56 @@ object App {
       .config("spark.redis.port", "6379")
       .getOrCreate()
 
-    val sc = spark.sparkContext
+    //load
+
+    redis_load(recordName, spark)
+
+    //write
+    redis_write(recordName, tableName, spark)
+
+
+    //read after write
+    redis_read(recordName, tableName, spark)
 
   }
 
+  private def redis_read(recordName: String, tableName: String, spark: SparkSession) = {
+    val df = spark.read.format("csv").option("header", "true").load(recordName)
+    time(s"Read") {
+      spark.read
+        .format("org.apache.spark.sql.redis")
+        .option("table", tableName)
+        .load()
+    }
+  }
+
+  private def redis_load(recordName: String, spark: SparkSession) = {
+    time(s"Load") {
+      val df = spark.read.format("csv").option("header", "true").load(recordName)
+    }
+  }
+
+  private def redis_write(recordName: String, tableName: String, spark: SparkSession) = {
+    val df = spark.read.format("csv").option("header", "true").load(recordName)
+    time(s"Write") {
+      df.write
+        .format("org.apache.spark.sql.redis")
+        .option("table", tableName)
+        .mode(org.apache.spark.sql.SaveMode.Overwrite)
+        .save()
+    }
+  }
+
+  def time[R](tag: String)(block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    new PrintWriter(new FileWriter(s"$benchmarkReportDir/results.txt", true)) {
+      // scalastyle:off
+      //Tag,  Duration representing a number of nanoseconds.
+      this.println(s"$tag, ${JDuration.ofNanos(t1 - t0)}")
+      close()
+    }
+    result
+  }
 }
