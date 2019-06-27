3420e81b73bc66c40e62912e0e614e736c9e8bc3
filipecosta90 <filipecosta.90@gmail.com>
2019-06-27 16:13:09 +0100

    [add] added config parameters for new test case

--- src/main/scala/com/fcosta_oliveira/App.scala
+++ src/main/scala/com/fcosta_oliveira/App.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.redis.{SqlOptionLogInfoVerbose, SqlOptionModel, SqlO
 import org.apache.spark.sql.types.{StructField, StructType, _}
 import org.apache.spark.sql.{Row, SparkSession}
 import org.slf4j.LoggerFactory
+import redis.clients.jedis.{JedisPool, JedisPoolConfig}
 
 import scala.util.Random
 
@@ -36,24 +37,35 @@ object App {
 
     // Distribute a local Scala collection to form an RDD.
 
+    val hostip = "localhost"
+    val hostport = 6379
+    val dbnum = 0
+
+    val pool = new JedisPool(new JedisPoolConfig, hostip, hostport, 5000)
+    val jedis = pool.getResource
+    jedis.flushDB
+    jedis.close()
+
     val spark = SparkSession
       .builder()
       .appName("spark-redis-profiling")
       .master("local[*]")
-      .config("spark.redis.host", "localhost")
-      .config("spark.redis.port", "6379")
+      .config("spark.redis.host", hostip)
+      .config("spark.redis.port", hostport.toString)
+      .config("spark.redis.kryoserializer.buffer.mb","32")
       .config("spark.dynamicAllocation.enabled", false)
       // Class to use for serializing objects that will be sent over the network or need to be cached in serialized form.
       .config("spark.serializer","org.apache.spark.serializer.KryoSerializer")
       // Initial size of Kryo's serialization buffer, in KiB unless otherwise specified.
       // Note that there will be one buffer per core on each worker.
-      .config("spark.kryoserializer.buffer","24m")
+      .config("spark.kryoserializer.buffer","2m")
       .getOrCreate()
 
     //val df: DataFrame = create_df(rowsNum, partitionsNum, schema _, spark )
     // used https://github.com/filipecosta90/spark-redis-datagen to generate records_rec_100000_col_400_dsize_36.parquet
     // python datagen.py --format parquet --records 100000
     val df = spark.read.parquet("data/records_rec_100000_col_400_dsize_36.parquet")
+
 
     time {
       df.write.format("org.apache.spark.sql.redis")
@@ -62,15 +74,15 @@ object App {
         .option(SqlOptionLogInfoVerbose, true)
         .save()
     }
-
-    time {
-      spark.read
-        .format("org.apache.spark.sql.redis")
-        .option("table", tableName)
-        .option(SqlOptionModel, SqlOptionModelBlock)
-        .option(SqlOptionLogInfoVerbose, true)
-        .load().foreach{_=>}
-    }
+//
+//    time {
+//      spark.read
+//        .format("org.apache.spark.sql.redis")
+//        .option("table", tableName)
+//        .option(SqlOptionModel, SqlOptionModelBlock)
+//        .option(SqlOptionLogInfoVerbose, true)
+//        .load().foreach { _ => }
+//    }
   }
 
   private def create_df(rowsNum: Int,partitionsNum: Int, schema: () => StructType, spark: SparkSession) = {
